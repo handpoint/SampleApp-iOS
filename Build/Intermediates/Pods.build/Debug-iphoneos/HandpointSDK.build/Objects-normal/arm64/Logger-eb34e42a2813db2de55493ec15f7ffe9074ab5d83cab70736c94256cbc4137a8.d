a1e0838231eac0006afe378930f5f2c4224e22c2
Juan Núñez <juan@handpoint.com>
2020-03-17 19:10:33 +0100

    Minor UI fixes

---
+++ Build/Intermediates/Pods.build/Debug-iphoneos/HandpointSDK.build/Objects-normal/arm64/Logger-eb34e42a2813db2de55493ec15f7ffe9074ab5d83cab70736c94256cbc4137a8.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/juan/repos/SampleApp-iOS/Pods/HandpointSDK/Library/heft/Shared/Logger.mm \
+  /Users/juan/repos/SampleApp-iOS/Pods/Target\ Support\ Files/HandpointSDK/HandpointSDK-prefix.pch \
+  /Users/juan/repos/SampleApp-iOS/Pods/HandpointSDK/Library/heft/Shared/Logger.h
---
+++ Build/Intermediates/Pods.build/Debug-iphoneos/HandpointSDK.build/Objects-normal/arm64/Logger-eb34e42a2813db2de55493ec15f7ffe96466de6550ec0c5c115ed827484bdef9.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/juan/repos/SampleApp-iOS/Pods/HandpointSDK/Library/heft/Shared/Logger.m \
+  /Users/juan/repos/SampleApp-iOS/Pods/Target\ Support\ Files/HandpointSDK/HandpointSDK-prefix.pch
